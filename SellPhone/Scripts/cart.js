fb43d91a0dc460c462e061ca2a24eb9a2bf44aa8
Le Tuan Anh <tuananh0333@gmail.com>
2020-07-06 17:32:13 +0700

    cart layout

---
+++ SellPhone/Scripts/cart.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $(".delete-row").on("click", function () {
+        var id = this.id;
+        var selector = this.parent().siblings().eq(1).child();
+
+        removeCartVal(id, selector);
+    });
+});
+
+function removeCartVal(id, selector) {
+    var num = selector.text();
+
+    $.ajax({
+        type: 'POST',
+        url: '/Cart/removeCart',
+        data: {
+            id: id
+        },
+        success: function () {
+            num--;
+            if (num == 0) {
+                selector.parents(".col-md-12").remove();
+            } else {
+                selector.text(num);
+            }
+        }
+    });
+}
